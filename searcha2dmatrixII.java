f2058b95ec241bd5591939eae1937ed34745eb31
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-05-11 14:38:01 -0400

    Create searcha2dmatrixII.java

---
+++ searcha2dmatrixII.java
@@ -0,0 +1,23 @@
+
+//240
+//medium
+
+//Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+//Integers in each row are sorted in ascending from left to right.
+//Integers in each column are sorted in ascending from top to bottom.
+
+// my own solution using java:
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
