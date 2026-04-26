fe82b30e3459c5a374ff9d2c1d84acf231af8fa8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2026-04-26 14:47:55 -0700

    Create ToLowerCase.java

---
+++ ToLowerCase.java
@@ -0,0 +1,21 @@
+//709
+//Easy
+
+//Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+ 
+
+//Example 1:
+
+//Input: s = "Hello"
+//Output: "hello"
+
+
+// my own solution using java:
+
+// just use the built in toLowerCase() method
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
