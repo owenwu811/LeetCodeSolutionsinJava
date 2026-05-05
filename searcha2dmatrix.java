1108e7a982d8b9cb3638303a8b8681eb21721e93
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2026-05-04 20:00:33 -0700

    Update searcha2dmatrix.java

--- searcha2dmatrix.java
+++ searcha2dmatrix.java
@@ -24,3 +24,28 @@ class Solution {
         return false;
     }
 }
+
+// my own optimized solution using java:
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            int l = 0;
+            int r = row.length - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (matrix[i][mid] == target) {
+                    return true;
+                }
+                else if (matrix[i][mid] > target) {
+                    r = mid - 1;
+                }
+                else {
+                    l = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+}
